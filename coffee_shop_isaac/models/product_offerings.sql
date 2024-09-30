094b12e21fa240e3965cb61cc2d3e5a234f99f38
Isaac Lello-Smith <35873591+ilellosmith@users.noreply.github.com>
2024-09-30 20:54:24 +0000

    removing models not useful for current exercises

--- coffee_shop_isaac/models/product_offerings.sql
+++
@@ -1,9 +0,0 @@
-select 
-  products.category as category,
-  count(distinct(products.name)) as n_products,
-from 
-  {{source('coffee_shop','products')}} products
-group by 
-  category
-order by 
-  n_products desc
